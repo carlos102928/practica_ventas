6b090424d04a10350980ed0ada31644bd0d85579
Al3j00529 <19alejandro.martinez2005@gmail.com>
2025-05-22 13:36:04 -0500

    rutas

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ import cors from 'cors';
 import express from 'express';
 import clienteRoutes from './routes/clienteRoutes.js';
 import productoRoutes from './routes/productoRoutes.js';
+import ventaRoutes from './routes/ventaRoutes.js';
 
 const app = express()
 app.use(cors());
@@ -9,7 +10,8 @@ app.use(express.json());
 app.use(express.static('../frontend'));
 
 app.use('/api/clientes', clienteRoutes);
-app.use('/api/productos', productoRoutes)
+app.use('/api/productos', productoRoutes);
+app.use('/api/ventas', ventaRoutes)
 
 const PORT = 3000;
 
